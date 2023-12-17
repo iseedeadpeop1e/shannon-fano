05ddc274df325414c74a6b9ab81bca453869faa1
iseedeadpeop1e <125872837+iseedeadpeop1e@users.noreply.github.com>
2023-12-18 00:50:47 +1000

    add origins

--- src/shfano/main.py
+++ src/shfano/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from algorithm import encode
 
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/index')
@@ -17,4 +28,4 @@ async def encode_text(text: str):
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run(app, host='127.0.0.1', port=8000)
